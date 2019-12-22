1716f456d419d8f33f515c2bb3524327f23a077c
IgorBarbashov <bis200705@gmail.com>
2019-12-22 18:53:37 +0300

    feature: 09-functions-n-closures-tasks - all tasks

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -179,8 +179,8 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn.call(null, ...[...args1, ...args2]);
 }
 
 
@@ -201,8 +201,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let increment = -1;
+  return () => {
+    increment += 1;
+    return startFrom + increment;
+  };
 }
 
 
